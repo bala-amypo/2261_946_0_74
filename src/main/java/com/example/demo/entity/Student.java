4aa8c49731467e3598b5f08cea1b5c259c85f8da
bala-amypo <bala@amypo.in>
2025-12-16 08:18:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -30,10 +30,13 @@ public class Student{
     public void setCgpa(long id){
         this.cgpa=cgpa;
     }
-   public Student(int id, String name, String email, float cgpa) {
+    public Student(int id, String name, String email, float cgpa) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.cgpa = cgpa;
     }
+     public Student() {
+    }
+    
 }

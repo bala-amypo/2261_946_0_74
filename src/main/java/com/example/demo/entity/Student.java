31b7857c27a577160da9c09c7b0705b7c8ebc33f
bala-amypo <bala@amypo.in>
2025-12-17 09:01:59 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -2,7 +2,29 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 // import jakarta.persistence.GeneratedValue;
-// import jakarta.p
+// import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private int id;
+    private String stuname;
+    private String stumail;
+    private float cgpa;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getStuname() {
+        return stuname;
+    }
+    public void setStuname(String stuname) {
+        this.stuname = stuname;
+    }
+    public String getStumail() {
         return stumail;
     }
     public void setStumail(String stumail) {
@@ -14,5 +36,13 @@ import jakarta.persistence.*;
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
     }
+    public Student(int id, String stuname, String stumail, float cgpa) {
+        // this.id = id;
+        this.stuname = stuname;
+        this.stumail = stumail;
+        this.cgpa = cgpa;
+    }
+    public Student() {
+        
     }
 }
